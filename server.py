96e568d8cbb2d37a16aced222d7b109c0b05ae69
Drew Jordan <ajordan12@luc.edu>
2023-06-24 09:14:07 -0500

    switched to flask from django

--- server.py
+++ server.py
@@ -3,24 +3,33 @@ import main as main # Import main.py
 import flask as flask
 from flask import Flask, render_template, request, jsonify, Response, json
 from flask.views import View
-# sesnor data class 
-class SensorData(View):
-    def get(self, request):
-        soilMoisture = main.getSoilMoisture()
-        light = main.getLight()
-        tempHumid = main.getTempHumid()
-        data = {
-            "Soil Moisture: " : soilMoisture,
-            "Light: " : light,
-            "Temperature: " : tempHumid
-        }
-        return json(data)
-    
-# video class
-class Video(View):
-    def get(self, request):
-        video = main.getVideo()
-        data = {
-            "Video: " : video
-        }
-        return json(data)
+
+# flask app
+app = Flask(__name__)
+
+# routes for the soil sensor
+@app.route('/soil-moisture' , methods=['GET'])
+def getSoilMoisture():
+    getSoilMoisture = main.getSoilMoisture()
+    return jsonify(getSoilMoisture)
+
+# routes for the light sensor
+@app.route('/light' , methods=['GET'])
+def getLight():
+    getLight = main.getLight()
+    return jsonify(getLight)
+
+# routes for the temperature and humidity sensor
+@app.route('/temp-humid' , methods=['GET'])
+def getTempHumid():
+    getTempHumid = main.getTempHumid()
+    return jsonify(getTempHumid)
+
+# routes for the video
+@app.route('/video' , methods=['GET'])
+def getVideo():
+    getVideo = main.getVideo()
+    return jsonify(getVideo)
+
+if __name__ == '__main__':
+    app.run()
