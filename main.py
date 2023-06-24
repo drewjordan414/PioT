de9f658000697e52afcd7d9b0e5d840278f4ab5d
Drew Jordan <ajordan12@luc.edu>
2023-06-23 20:43:41 -0500

    gpio support

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-import adafruit_seesaw
+# import libraries
+import adafruit_seesaw as ss
+import os 
+import RPI.GPIO as GPIO
 import board
 import busio
 import adafruit_sht4x
 import adafruit_tsl2591
+import cv2 as cv
+import smbus2
+import json 
+
+# gpio warnings off
+GPIO.setwarnings(False)
+
+# gpio pins for the realy
+GPIO.setmode(GPIO.BCM)
+pinlist = [17, 22, 27, 10]
+for i in pinlist:
+    GPIO.setup(i, GPIO.OUT)
+    GPIO.output(i, GPIO.HIGH)
+
+# create i2c bus
+i2c_bus = busio.I2C(board.SCL, board.SDA)
 
